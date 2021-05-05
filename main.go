e75ff33d6d7a2b6631137130288fcf2c830003c8
Sharan Aithal <32029982+sharan-aithal@users.noreply.github.com>
2021-05-05 20:52:48 +0530

    get unix time

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 
 func main() {
 	fmt.Println("Now:", Now())
+	fmt.Println("Unix:", Unix())
 }
 
 func Now() time.Time {
 	return time.Now()
+}
+
+func Unix() int64 {
+	return time.Now().Unix()
 }
